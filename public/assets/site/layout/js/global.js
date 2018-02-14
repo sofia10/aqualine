a368b2991ac9e438d19b9c0024217baafbd6b8d4
sofia10 <makharashvilisofachka@gmail.com>
2018-02-14 11:45:07 +0400

    last edit

--- public/assets/site/layout/js/global.js
+++ public/assets/site/layout/js/global.js
@@ -1,3 +1,19 @@
-$('.search-btn').click(function(){
-    $('.search-btn').toggleClass('active');
+$('.product_clicker').click(function(ev){
+	ev.preventDefault();
+	$('.nav-men').fadeToggle('slow');
+});
+$('.inner-dropdown a').click(function(ev){
+	ev.preventDefault();
+	$('.inner-nav').fadeToggle('slow');
+});
+
+var counter = 0;
+$('.nav-open').click(function(){
+	if(counter == 0){
+		$('body').css('overflow', 'hidden');
+		counter++;
+	}else{
+		$('body').css('overflow-y', 'scroll');
+		counter = 0;
+	}
 });
